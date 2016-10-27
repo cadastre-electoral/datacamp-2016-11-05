29cd96544b875a342055e2f92cb75879fd1deaa8
Christian Quest <cquest@openstreetmap.fr>
2016-10-27 18:40:51 +0200

    un peu de rangement...

--- cquest/arretes/pdf2tiff.sh
+++ cquest/arretes/pdf2tiff.sh
@@ -1,8 +1,9 @@
 # conversion des fichiers PDF en TIFF exploitables par tesseract-ocr
 # dépend de ImageMagick
 
+mkdir -p tiff
 for f in ../../ARRETES*/*.pdf
 do
   convert -density 300 "$f" -type Grayscale -background white +matte "$f.tiff"
-  mv "$f.tiff" ./
+  mv "$f.tiff" ./tiff
 done
