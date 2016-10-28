f62352b51d316b48ebe3b7be5f6bcb9e8a52d309
Christian Quest <cquest@openstreetmap.fr>
2016-10-28 15:27:48 +0200

    conversion TIFF > TXT avec tesseract

---
+++ cquest/arretes/2_tiff2txt.sh
@@ -0,0 +1,23 @@
+# conversion des fichiers TIFF multipage en TXT par tesseract-ocr
+# comme les orientations varient d'une page à l'autre pour chaque PDF,
+# les pages sont traitées une à une
+
+for t in 1-tiff/*.pdf.tiff
+do
+  echo $t
+  rm -f /tmp/tiff2txt-*.tif
+  # conversion multipage en pages uniques numérotées 00..nn par ImageMagick
+  convert "$t" /tmp/tiff2txt-%02d.tif
+  rm -f "$t.txt"
+  for p in /tmp/tiff2txt-*.tif
+  do
+    echo $p
+    # OCR avec tesseract
+    tesseract $p stdout -l fra -psm 1 >> "$t.txt"
+    rm $p
+  done
+done
+
+# on range le résultat dans le dossier "2-txt"
+mkdir -p 2-txt
+mv tiff/*.pdf.tiff.txt 2-txt
